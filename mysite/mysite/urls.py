5b22b72b3d66479f14c250b164fedfea6cb92a6f
Kimmyeonghyeon0 <Kimmyeonghyeom0>
2024-11-07 18:23:50 +0900

    django 1일차 구구단

---
+++ mysite/mysite/urls.py
@@ -0,0 +1,36 @@
+"""
+URL configuration for mysite project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.shortcuts import render, redirect
+# from django.Http import Http404, HttpResponse
+
+def gugu(request, num):
+    if num <2:
+        return redirect('/gugu/2/')
+
+    context = {
+        'dan': num,
+        'results': [(i, num * i) for i in range(1, 10)]
+    }
+
+    return render(request, template_name= 'gugu.html', context)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('gugu/<int:num>/', gugu),
+]
